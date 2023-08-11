8e8624ea21bc0e8d491cf3fd86cb3298fa2af6f3
Frank Tennant <frank.tennant@wmg.com>
2023-08-11 15:55:01 +0100

    second commit

--- routes/shows.js
+++ routes/shows.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 const { User, Show } = require("../models/index")
+const checker = async function (req, res, next) {
+    const parameter = await String(req.params.status);
+    console.log(parameter);
+    if (req.params.status === null || req.params.status === undefined) {
+        throw new Error
+    };
+    next()
+};
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
+router.use(checker);
 
 router.get("/", async (req, res) => {
     const shows = await Show.findAll();
@@ -30,7 +40,11 @@ router.put("/:id/rating/:rating", async (req, res) => {
     res.json(show);
 });
 
-router.put("/:id/status/:status", async (req, res) => {
+router.put("/:id/status/:status", checker, async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    res.json({error: errors.array()});
+  } else {
     await Show.update({status: req.params.status}, {
         where:{
             id: req.params.id
@@ -38,6 +52,7 @@ router.put("/:id/status/:status", async (req, res) => {
     });
     const show = await Show.findByPk(req.params.id)
     res.json(show);
+  };
 });
 
 router.delete("/:id", async (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 const { User, Show } = require("../models/index")
 
 router.use(express.json());
